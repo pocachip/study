33f683ff7f115613c51fec4e237fb74f85be2439
pocachip <parkseou@gmail.com>
2018-09-20 18:24:21 +0900

    last

--- TypeScript/1. basic_type.ts
+++ TypeScript/1. basic_type.ts
@@ -39,10 +39,10 @@ xx = ["hello", 10]; // OK
 //x = [10, "hello"]; //Error
 
 // accessing
-console.log(x[0].substr(1)); //OK
+console.log(xx[0].substr(1)); //OK
 //console.log(x[1].substr(1)); //Error, 'number' does not have 'substr'
-x[3] = "world"; //OK, 'string' can be assigned to 'string | number'
-console.log(x[5].toString()); //OK, 'string' and 'number' both have 'toString'
+xxx[3] = "world"; //OK, 'string' can be assigned to 'string | number'
+console.log(xxx[5].toString()); //OK, 'string' and 'number' both have 'toString'
 //x[6] = true; // Error, 'boolean' isn't 'string|number'
 
 // Enum
